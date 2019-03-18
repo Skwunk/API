b451813c4af8d2bfada3dba6c417d5516474494c
Jordan Cameron <jordan.cameron.vigo@gmail.com>
2019-03-18 19:25:52 +0000

    Changed the import in main.go

--- main.go
+++ main.go
@@ -12,15 +12,7 @@ package main
 import (
 	"log"
 	"net/http"
-
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//    sw "github.com/myname/myrepo/go"
-	//
-	sw "./go"
+	sw "github.com/Skwunk/API/go"
 )
 
 func main() {
